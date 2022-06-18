a150baafaa98702a256174ba73ca75a443490056
cei0907 <cei0907@naver.com>
2022-06-18 17:55:30 +0900

    에셋 추가 및 게임화면 구성

---
+++ 2DRhythmGame_Exer/Assets/Scripts/BackgroundBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundBehavior : MonoBehaviour
+{
+
+    public GameObject gameBackground;
+    private SpriteRenderer gameBackgroundSpriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameBackgroundSpriteRenderer = gameBackground.GetComponent<SpriteRenderer>();
+        StartCoroutine(FadeOut(gameBackgroundSpriteRenderer, 0.005f));
+        
+    }
+
+    IEnumerator FadeOut(SpriteRenderer spriteRenderer, float amount)
+    {
+        Color color = spriteRenderer.color;
+        while(color.a > 0.0f)
+        {
+            color.a -= amount;
+            spriteRenderer.color = color;
+            yield return new WaitForSeconds(amount);//0.005f만큼 쉬었다가 다시 실행하게 해주는 것.. 이것이 yield 키워드인지 조사해보자
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
